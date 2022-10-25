a5314a3e41d03f8f036f3b82bb3756803e7ae166
Vitor Bonella <51213068+VitorBonella@users.noreply.github.com>
2022-10-25 16:51:47 -0300

    AvailableTime dumb

--- Agenda.hs
+++ Agenda.hs
@@ -16,8 +16,27 @@ type DayOff = (Month,Day)
 
 type Year = (Bool, [DayOff])
 
+getHour (h , _) = h
+getTime (_ , t) = t
 
+-- Verify wheter the requested time is free
+verifyAvailableSchedule ((h,t):ss) schedule
+    | h == (getHour schedule) = False
+    | null ss = True
+    | otherwise verifyAvailableSchedule ss schedule
+
+-- Verify wheter the day is util and find the requested day
+verifyAvailableDay ((d,ss):ds) schedule day
+    | d == day = verifyAvailableSchedule ss schedule
+    | null ds = False
+    | otherwise = verifyAvailableDay ds schedule day
+
+-- Verify whether the schedule time is available, this funcition search first for the month in agenda
 AvailableTime :: Agenda -> Year -> Day -> Month -> Scheduling -> Bool
+AvailableTime ((b,m,ds):agenda) yearInfo month day schedule 
+    | m == month = verifyAvailable ds schedule day
+    | null agenda = False
+    | otherwise = AvailableTime agenda yearInfo month day schedule
 
 
 main = do
