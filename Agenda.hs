8ab7188fa731718b30042319fcbcd1e2e8c59045
Vitor Bonella <51213068+VitorBonella@users.noreply.github.com>
2022-10-26 15:02:13 -0300

    Read Calendario

--- Agenda.hs
+++ Agenda.hs
@@ -7,14 +7,37 @@ type Time = Int
 
 type Scheduling = (Hour,Time)
 
-type Day = (Int, [Scheduling)])
+type Day = (Int, [Scheduling])
 
-type Month = Int
-type Agenda = (Bool, Month, [Day])
+type Month = (Bool, Int, [Day])
+type Agenda = [Month]
 
-type DayOff = (Month,Day)
+type DayOff = [Bool]
 
 type Year = (Bool, [DayOff])
+
+agenda yearDaysOff agendaData = do
+    
+    showMenu
+    op <- leOp
+
+    if op == 0 then
+        putStrLn "Saindo da Agenda"
+    else
+        agenda yearDaysOff agendaData
+
+main = do
+    yearDaysOff <- readYear
+    -- putStrLn (show year)
+
+    agendaData <- initAgenda
+    putStrLn (show agendaData)
+
+    agenda yearDaysOff agendaData
+
+
+
+
 
 getHour (h , _) = h
 getTime (_ , t) = t
@@ -23,7 +46,7 @@ getTime (_ , t) = t
 verifyAvailableSchedule ((h,t):ss) schedule
     | h == (getHour schedule) = False
     | null ss = True
-    | otherwise verifyAvailableSchedule ss schedule
+    | otherwise = verifyAvailableSchedule ss schedule
 
 -- Verify wheter the day is util and find the requested day
 verifyAvailableDay ((d,ss):ds) schedule day
@@ -32,14 +55,43 @@ verifyAvailableDay ((d,ss):ds) schedule day
     | otherwise = verifyAvailableDay ds schedule day
 
 -- Verify whether the schedule time is available, this funcition search first for the month in agenda
-AvailableTime :: Agenda -> Year -> Day -> Month -> Scheduling -> Bool
-AvailableTime ((b,m,ds):agenda) yearInfo month day schedule 
-    | m == month = verifyAvailable ds schedule day
+-- AvailableTime :: Agenda -> Year -> Day -> Month -> Scheduling -> Bool
+availableTime ((b,m,ds):agenda) yearInfo month day schedule 
+    | m == month = verifyAvailableDay ds schedule day
     | null agenda = False
-    | otherwise = AvailableTime agenda yearInfo month day schedule
+    | otherwise = availableTime agenda yearInfo month day schedule
 
 
-main = do
-    showMenu
-    op <- leOp
-    main
+-- ############################## AGENDA HANDLERS ##############################
+
+--initDayList = [ (x, []) | x <- [1..31] ]
+
+initAgenda = return [(False,i,[] :: Day) | i <- [1..12]]
+
+-- ############################## INSERT A SCHEDULE ##############################
+
+
+
+-- ############################## READING THE YEAR DAYS OFF ##############################
+
+-- map read $ words string :: [Int] faz "1 2 3" virar [1,2,3]
+stringToList string = map read $ words string :: [Int]
+
+-- Test whether the day is in the list of days off
+testPosition d (x:ds)
+    | d == x = False
+    | (null ds) = True
+    | otherwise = testPosition d ds 
+
+convertToDayOff string = [ testPosition d (stringToList string) | d <- [1..31]]
+
+-- True = Util Day , False = Holiday or weekend
+readDayOffs ds = [convertToDayOff x| x <- ds]
+
+divideYearInput (x:xs) = (x,readDayOffs xs)
+
+-- Read Year
+readYear = do 
+    content <- readFile "calendario.txt"
+    --putStrLn (show (lines (content)))
+    return (divideYearInput (lines (content)))   
